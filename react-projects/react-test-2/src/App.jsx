39a89a77ccd6c26da4b3607a8c9da1a83a1cf349
Developer Shakib Ahmed <md.shakibahmed2015@gmail.com>
2023-08-20 13:45:58 +0600

    Add React Test Project

--- react-projects/react-test-2/src/App.jsx
+++ react-projects/react-test-2/src/App.jsx
@@ -1,9 +1,7 @@
-import { useState } from 'react';
-import './App.css';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-
-import Test from "./Test";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -24,14 +22,12 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-
-      <Test/>
     </>
   )
 }
